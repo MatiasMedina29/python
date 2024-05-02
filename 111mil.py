7268d24dbd64223c37783caac7484aef270abf5b
MatiasMedina29 <matiasmedina2909@gmail.com>
2024-05-02 09:49:06 -0300

    Update 111mil.py

---
+++ 111mil.py
@@ -0,0 +1,11 @@
+ingreso=int(input("Ingrese la posicion del corredor: "))
+if ingreso==1: 
+    print("Entregar medalla de oro")
+else:
+    if ingreso==2:
+        print("Entregar medalla de plata")
+    else:
+        if ingreso==3:
+            print("Entregar medalla de bronce")
+
+
